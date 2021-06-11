10211d5295c0bab3c0a98977acf85b58b3ff8dc3
srini <srinivaschannakeshava@gmail.com>
2021-06-11 13:36:29 +0530

    aspect updates

--- src/main/java/in/srini91/learn/config/aspect/FlightAspect.java
+++ src/main/java/in/srini91/learn/config/aspect/FlightAspect.java
@@ -19,7 +19,7 @@ public class FlightAspect {
 	private Flyer2 flyer2;
 
 	/*
-	 * this example demonstrates the usefull ness of spring AoP introduction which
+	 * this example demonstrates the usefulness of spring AoP introduction which
 	 * helps in extending the features of a old implementation without touching the
 	 * source code. Here we take a simple example of Flight class whose features is
 	 * extended via AoP introduction concept with flyerImpl class.
